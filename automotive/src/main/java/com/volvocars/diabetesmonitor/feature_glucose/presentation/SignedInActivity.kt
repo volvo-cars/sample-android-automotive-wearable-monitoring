edd421c328c2d370e329d74b6a23a0bc3be26eb3
Niclas <niclas.lind93@gmail.com>
2022-05-06 10:28:04 +0200

    trying to use carui from google

--- automotive/src/main/java/com/volvocars/diabetesmonitor/feature_glucose/presentation/SignedInActivity.kt
+++ automotive/src/main/java/com/volvocars/diabetesmonitor/feature_glucose/presentation/SignedInActivity.kt
@@ -1,70 +1,11 @@
-package com.volvocars.diabetesmonitor.feature_glucose.presentation
+package com.volvocars.diabetesmonitor
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.FragmentActivity
-import com.android.car.ui.core.CarUi
-import com.android.car.ui.toolbar.MenuItem
-import com.android.car.ui.toolbar.Toolbar
-import com.android.car.ui.toolbar.ToolbarController
-import com.volvocars.diabetesmonitor.R
-import com.volvocars.diabetesmonitor.core.util.showFragment
-import com.volvocars.diabetesmonitor.core.util.tryRemoveFragment
-import com.volvocars.diabetesmonitor.databinding.ActivitySignedInBinding
-import com.volvocars.diabetesmonitor.feature_glucose.presentation.diabetes_monitor.DiabetesMonitorFragment
-import com.volvocars.diabetesmonitor.feature_glucose.presentation.settings.DiabetesSettingsFragment
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class SignedInActivity : FragmentActivity() {
-
-    private lateinit var binding: ActivitySignedInBinding
-
-    private lateinit var toolbarController: ToolbarController
-
+class SignedInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignedInBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        initToolbar()
-
-        supportFragmentManager.showFragment(DiabetesMonitorFragment(), binding.frameLayoutS)
-    }
-
-    override fun onBackPressed() {
-        Log.d(TAG, "onBackPressed: ${toolbarController.state}")
-        when (toolbarController.state) {
-            Toolbar.State.SUBPAGE -> {
-                removeFragmentOrFinish()
-            }
-            else -> toolbarController.state = Toolbar.State.SUBPAGE
-        }
-    }
-
-    private fun removeFragmentOrFinish() {
-        if (!supportFragmentManager.tryRemoveFragment(binding.frameLayoutS)) {
-            finish()
-        }
-    }
-
-
-    private fun initToolbar() {
-        toolbarController = CarUi.requireToolbar(this)
-        toolbarController.setTitle(R.string.app_name)
-        toolbarController.state = Toolbar.State.HOME
-        toolbarController.setLogo(R.mipmap.diabetes_monitor_ic_launcher_round)
-        toolbarController.setMenuItems(initMenuItems())
-    }
-
-    private fun initMenuItems() = listOf<MenuItem>(
-        MenuItem.builder(this).setToSettings().setOnClickListener {
-            supportFragmentManager.showFragment(DiabetesSettingsFragment(), binding.frameLayoutS)
-            toolbarController.state = Toolbar.State.SUBPAGE
-            toolbarController.setMenuItems(listOf())
-        }.build()
-    )
-
-    companion object {
-        val TAG = SignedInActivity::class.simpleName
+        setContentView(R.layout.activity_signed_in2)
     }
 }
