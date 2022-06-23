cbf1f57cbb4d56038d3580f31c2e5b229bd7dfea
Niclas <niclas.lind93@gmail.com>
2022-06-23 16:46:54 +0200

    replaced retrofit with ktor

--- automotive/src/main/java/com/volvocars/wearable_monitor/di/NetworkModule.kt
+++ automotive/src/main/java/com/volvocars/wearable_monitor/di/NetworkModule.kt
@@ -1,66 +1,69 @@
 package com.volvocars.wearable_monitor.di
 
-import com.volvocars.wearable_monitor.core.util.Constants
+import android.util.Log
 import com.volvocars.wearable_monitor.feature_glucose.data.remote.NightScoutApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import retrofit2.Converter
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.features.observer.*
+import io.ktor.client.request.*
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    /**
-     * @return an [GsonConverterFactory] instance
-     */
-    @Provides
-    @Singleton
-    fun provideConverterFactory(): Converter.Factory = GsonConverterFactory.create()
-
-    /**
-     * @return an [OkHttpClient] instance
-     */
     @Singleton
     @Provides
-    fun providesOkHttpClient(
-    ) = OkHttpClient().apply {
-        OkHttpClient.Builder().run {
-            callTimeout(20, TimeUnit.SECONDS)
-            connectTimeout(20, TimeUnit.SECONDS)
-            writeTimeout(20, TimeUnit.SECONDS)
-            readTimeout(20, TimeUnit.SECONDS)
-            build()
-        }
+    fun provideNightScoutApiV2(client: HttpClient): NightScoutApi {
+        return NightScoutApi(client)
     }
 
-    @Provides
     @Singleton
-    fun provideRetrofit(
-        gson: Converter.Factory,
-        client: OkHttpClient,
-    ): Retrofit =
-        Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(gson)
-            .client(client)
-            .build()
+    @Provides
+    fun provideKtorClient(): HttpClient {
+        return HttpClient(Android) {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
 
-    /**
-     * @param retrofit [Retrofit] instance
-     * @return [NightScoutApi] impl
-     */
-    @Singleton
-    @Provides
-    fun providesNightScoutApiService(
-        retrofit: Retrofit,
-    ): NightScoutApi {
-        return retrofit.create(NightScoutApi::class.java)
+                engine {
+                    connectTimeout = 60_000
+                    socketTimeout = 60_000
+                }
+            }
+
+            install(Logging) {
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.v("Logger Ktor => ", message)
+                    }
+                }
+                level = LogLevel.ALL
+            }
+
+            install(ResponseObserver) {
+                onResponse {
+                    Log.d("HTTP status: ", "${it.status.value}")
+                }
+            }
+
+            install(DefaultRequest) {
+                header(
+                    io.ktor.http.HttpHeaders.ContentType,
+                    io.ktor.http.ContentType.Application.Json
+                )
+            }
+        }
     }
 }
